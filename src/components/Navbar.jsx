a19a4189c9dbf22f85b757c01ee4d76002bdf81b
Maverick Seneris <mavseneris@gmail.com>
2022-11-25 11:16:24 +0800

    added shopping bag

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -168,12 +168,136 @@ const Button = styled.button`
     cursor: pointer;  
 `
 const ShoppingBagWrapper = styled.div`
-    width: 27%;
+    width: 400px;
     height: 100vh;
     background-color: white;
     position: absolute;
     right: 0px;
 `
+
+
+//** Login Menu */ 
+const Form = styled.form`
+    border-top: 1px solid #d0d0d0;
+    padding: 60px 20px 0 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+`
+const Header = styled.h3`
+    font-size: 30px;
+    padding: 0 20px;
+    margin: 30px 0 10px 0;
+`
+
+const SubTitle = styled.div`
+    padding: 0 20px;
+    margin-bottom: 60px;
+    font-size: 14px;
+`
+const Label = styled.label`
+    font-size: 14px;
+`
+const InputLogin = styled.input`
+    width: 100%;
+    height: 42px;
+    border-radius: 25px;
+    cursor: pointer;
+    border: 1px solid black;
+    outline: none;
+    background-color: white;
+    padding: 20px;
+    font-size: 14px;
+`
+const Agreement = styled.p`
+    font-size: 16px;
+    margin: 10px 0;
+`
+const ButtonLogin = styled.button`
+    width: 100%;
+    height: 42px;
+    border-radius: 25px;
+    cursor: pointer;
+    border: 1px solid black;
+    outline: none;
+    background-color: black;
+    color: white;
+
+&:hover {
+    background-color: #191919;
+}
+`
+
+const FormLogin = styled.form`
+    padding: 10px 20px 0 20px;
+    display: flex;
+    flex-direction: column;
+    gap: 10px;
+    width: 100%;
+`
+
+const EmailLogin = styled.div`
+    padding: 0 20px;
+    margin-bottom: 20px;
+    font-size: 14px;
+`
+
+// * END
+
+//* Shopping cart 
+
+const Bag = styled.div`
+    margin-top: 5px;
+    padding: 8px 20px 0 20px;
+    display: flex;
+    gap: 13px;
+    font-size: 12px;
+`
+const ProductImage = styled.img`\
+    width: 30%;
+`
+const ProductInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+    width: 70%;
+`
+const ProductName = styled.h4``
+const ProductSize = styled.h4``
+const ProductColour = styled.h4``
+const AddContainer = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const AmountContainer = styled.div`
+    display: flex;
+    gap: 6px;
+    font-size: 16px;
+    align-items: center;
+`
+
+const AmountButton = styled.button`
+    border: none;
+    background-color: white;
+`
+const Amount = styled.span`
+    font-size: 12px;
+`
+const ProductAmountPrice = styled.div`
+    font-size: 12px;
+`
+
+const ViewProduct = styled.a`
+    text-decoration: underline;
+    font-size: 12px;
+`
+const Remove = styled.a`
+    text-decoration: underline;
+    font-size: 10px;c
+`
+
+
 
 export default function Navbar() {
     const [showSearch, setShowSearch] = useState(false)
@@ -212,59 +336,6 @@ export default function Navbar() {
         const { value } = event.target
         event.key === "Enter" && console.log(value) && setSearchValue(value);
     };
-
-    const Form = styled.form`
-        border-top: 1px solid #d0d0d0;
-        padding: 60px 20px 0 20px;
-        display: flex;
-        flex-direction: column;
-        gap: 10px;
-        width: 100%;
-    `
-    const Header = styled.h3`
-        font-size: 30px;
-        padding: 0 20px;
-        margin: 30px 0 10px 0;
-    `
-
-    const SubTitle = styled.div`
-        padding: 0 20px;
-        margin-bottom: 60px;
-        font-size: 14px;
-    `
-    const Label = styled.label`
-        font-size: 14px;
-    `
-    const Input = styled.input`
-        width: 100%;
-        height: 42px;
-        border-radius: 25px;
-        cursor: pointer;
-        border: 1px solid black;
-        outline: none;
-        background-color: white;
-        padding: 20px;
-        font-size: 14px;
-        `
-    const Agreement = styled.p`
-        font-size: 16px;
-        margin: 10px 0;
-    `
-    const Button = styled.button`
-        width: 100%;
-        height: 42px;
-        border-radius: 25px;
-        cursor: pointer;
-        border: 1px solid black;
-        outline: none;
-        background-color: black;
-        color: white;
-
-        &:hover {
-        background-color: #191919;
-    `
-
-
 
 
     return (
@@ -310,37 +381,125 @@ export default function Navbar() {
                 </ButtonContainer>
             </InputWrapper>}
 
-            {/* {showLogin && !showSearch && <LoginWrapper>
+            ${'' /* {showLogin && !showSearch && <LoginWrapper>
                 <Title>MAAV Profile</Title>
                 <Desc>Build your unique profile for faster checkout, see full order history, save riding preferences and get access to special offers, before the rest of the pack.</Desc>
                 <LoginFlexContainer>
                     <Email placeholder='email@example.com' />
                     <Button>Get Started</Button>
                 </LoginFlexContainer>
-            </LoginWrapper>} */}
+            </LoginWrapper>}  */}
 
             {showLogin && !showSearch && <LoginWrapper>
-                <ArrowBack fontSize='large'/>
+                <ArrowBack fontSize='large' cursor="pointer" />
                 <Header>One Last Step!</Header>
                 <SubTitle>Sit tight - just two minutes until your account is ready to go</SubTitle>
                 <Form>
                     <Label>Email</Label>
-                    <Input placeholder='email@example.com' />
+                    <InputLogin placeholder='email@example.com' />
                     <Label>First Name</Label>
-                    <Input placeholder='First Name' />
+                    <InputLogin placeholder='First Name' />
                     <Label>Last Name</Label>
-                    <Input placeholder='Last Name' />
+                    <InputLogin placeholder='Last Name' />
                     <Agreement>
                         By registering, I agree to the MAAV Terms & Conditions and Privacy Policy
                     </Agreement>
-                    <Button>Creat Profile</Button>
+                    <ButtonLogin>Creat Profile</ButtonLogin>
                 </Form>
             </LoginWrapper>}
 
+            ${'' /* {showLogin && !showSearch && <LoginWrapper>
+                <ArrowBack fontSize='large' cursor="pointer"/>
+                <Header>Please login</Header>
+                <EmailLogin>Email: email@example.com</EmailLogin>
+                <FormLogin>
+                    <Label>Password</Label>
+                    <InputLogin placeholder='Password' />
+                    
+                    <ButtonLogin>Login</ButtonLogin>
+                </FormLogin>
+            </LoginWrapper>} */}
 
-            {showBag && !showSearch && <ShoppingBagWrapper>
+
+            ${'' /* {showBag && !showSearch && <ShoppingBagWrapper>
                 <Title>Shopping Bag (0 items)</Title>
                 <Desc>Your Cart is currently empty.</Desc>
+            </ShoppingBagWrapper>} */}
+
+            {showBag && !showSearch && <ShoppingBagWrapper>
+                <Title>Shopping Bag (4 items)</Title>
+                <Bag>
+                    <ProductImage src="https://maap.cc/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0510%2F7809%2Fproducts%2FMAP-MAJ334_LAVA_EvadeProBaseJersey_Lava_PDP_SPECS_03_DESKTOP.jpg%3Fv%3D1668029203&w=1920&q=100" />
+                    <ProductInfo>
+                        <ProductName>Shift Pro Base Jersey</ProductName>
+                        <ProductSize>Size Extra Small</ProductSize>
+                        <ProductColour>Colour Spruce</ProductColour>
+                        <AddContainer>
+                            <AmountContainer>
+                                <AmountButton>+</AmountButton>
+                                <Amount>1</Amount>
+                                <AmountButton>-</AmountButton>
+                            </AmountContainer>
+                            <ProductAmountPrice>$115.00</ProductAmountPrice>
+                        </AddContainer>
+                        <ViewProduct>View Product</ViewProduct>
+                        <Remove>Remove</Remove>
+                    </ProductInfo>
+                </Bag>
+                <Bag>
+                    <ProductImage src="https://maap.cc/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0510%2F7809%2Fproducts%2FShiftProBaseJersey_Spruce_PDP_SPECS_03_DESKTOP.jpg%3Fv%3D1660192789&w=1920&q=100" />
+                    <ProductInfo>
+                        <ProductName>Shift Pro Base Jersey</ProductName>
+                        <ProductSize>Size Extra Small</ProductSize>
+                        <ProductColour>Colour Spruce</ProductColour>
+                        <AddContainer>
+                            <AmountContainer>
+                                <AmountButton>+</AmountButton>
+                                <Amount>1</Amount>
+                                <AmountButton>-</AmountButton>
+                            </AmountContainer>
+                            <ProductAmountPrice>$115.00</ProductAmountPrice>
+                        </AddContainer>
+                        <ViewProduct>View Product</ViewProduct>
+                        <Remove>Remove</Remove>
+                    </ProductInfo>
+                </Bag>
+                <Bag>
+                    <ProductImage src="https://maap.cc/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0510%2F7809%2Fproducts%2FMAP-MAB148_STK_ProBib2.0_Shitaki_PDP_SPECS_03_DESKTOP.jpg%3Fv%3D1668029203&w=1920&q=100" />
+                    <ProductInfo>
+                        <ProductName>Shift Pro Base Jersey</ProductName>
+                        <ProductSize>Size Extra Small</ProductSize>
+                        <ProductColour>Colour Spruce</ProductColour>
+                        <AddContainer>
+                            <AmountContainer>
+                                <AmountButton>+</AmountButton>
+                                <Amount>1</Amount>
+                                <AmountButton>-</AmountButton>
+                            </AmountContainer>
+                            <ProductAmountPrice>$115.00</ProductAmountPrice>
+                        </AddContainer>
+                        <ViewProduct>View Product</ViewProduct>
+                        <Remove>Remove</Remove>
+                    </ProductInfo>
+                </Bag>
+                <Bag>
+                    <ProductImage src="https://maap.cc/_next/image?url=https%3A%2F%2Fcdn.shopify.com%2Fs%2Ffiles%2F1%2F0510%2F7809%2Fproducts%2FMAP-MAA097_CLROLV_TrainingBottle_ClearOlive_PDP_HERO_01_DESKTOP.jpg%3Fv%3D1667427496&w=1920&q=100" />
+                    <ProductInfo>
+                        <ProductName>Shift Pro Base Jersey</ProductName>
+                        <ProductSize>Size Extra Small</ProductSize>
+                        <ProductColour>Colour Spruce</ProductColour>
+                        <AddContainer>
+                            <AmountContainer>
+                                <AmountButton>+</AmountButton>
+                                <Amount>1</Amount>
+                                <AmountButton>-</AmountButton>
+                            </AmountContainer>
+                            <ProductAmountPrice>$115.00</ProductAmountPrice>
+                        </AddContainer>
+                        <ViewProduct>View Product</ViewProduct>
+                        <Remove>Remove</Remove>
+                    </ProductInfo>
+                </Bag>
             </ShoppingBagWrapper>}
         </Container>
     )
